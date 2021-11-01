812daca22cb1768785b20db6b71663b1bfe489a0
tgilkerson <tgilkerson@miletwo.us>
2021-10-31 23:09:33 -0400

    order list from airtable

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import Navbar from "../comps/Navbar";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
--- pages/orders/index.js
+++ pages/orders/index.js
@@ -1,9 +1,53 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-export default withPageAuthRequired(function Orders({ user }) {
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from AirTable
+  const orders = await getOrders()
+
+  // Pass orders to the page via props
+  return { props: { orders } };
+}
+
+export default withPageAuthRequired(function Orders({ orders }) {
   return (
     <div>
       <h1>Orders</h1>
+      {orders.map((order) => (
+        <div key={order.RecID}>
+          <a>
+            <h3>OrderNo: {order.OrderNo}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 });
+
+////////////////////////////////////////////////////////////////////////////
+//          Get Orders
+////////////////////////////////////////////////////////////////////////////
+async function getOrders() {
+
+  var account = "tonygilkerson@gmail.com"                   //DEVTODO get from user profile
+  const apiKey = process.env.AIRTABLE_APIKEY
+  console.log("apikey %s", apiKey)
+  var Airtable = require("airtable")
+  
+  Airtable.configure({endpointUrl: "https://api.airtable.com",apiKey: apiKey,});
+
+  var base = Airtable.base("apptDZu7d1mrDMIFp"); //MRFC
+  const records = await base("Order").select({pageSize: 25, view: "fp-grid", filterByFormula: 'Account = "' + account + '"',}).all();
+
+  // Put resultes into an array
+  var orders = []; 
+  records.forEach(function (record) {
+    var order = record.fields;
+    orders.push(order);
+  });
+
+  return orders
+}
+
+
+
